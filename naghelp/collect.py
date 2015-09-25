8d5b45343b04bf413d16dd3dd9dc1693c3d487d8
Eric Lapouyade <elapouya@gmail.com>
2015-09-25 16:36:46 +0200

    new Expect class

--- naghelp/collect.py
+++ naghelp/collect.py
@@ -64,29 +64,44 @@ class NotConnected(Exception):
     pass
 
 class Expect(object):
-    def __init__(self,spawn,prompt,*steps,**kwargs):
+    KILL = 1
+
+    def __init__(self,spawn,login_steps,prompt,logout_steps,context={},timeout = 30,*args,**kwargs):
         #import is done only on demand, because it takes some little time
         import pexpect
-        self.in_with = False
         self.is_connected = False
-        self.prompt = None
-        if login_pattern_list is None:
-            login_pattern_list = [re.compile(r'login\s*:',re.I),]
-        if passwd_pattern_list is None:
-            passwd_pattern_list = [re.compile(r'Password\s*:',re.I),]
-        if prompt_pattern_list is None:
-            prompt_pattern_list = [re.compile(r'[\r\n][^\$#<>:]*[\$#>:]'),]
-        self.prompt_pattern_list = prompt_pattern_list
-        with Timeout(seconds = timeout, error_message='Timeout (%ss) for telnet to %s' % (timeout,host)):
-            self.tn = telnetlib.Telnet(host,port,timeout,**kwargs)
-            self.tn.expect(login_pattern_list)
-            self.tn.write(user + "\n")
-            self.tn.expect(passwd_pattern_list)
-            self.tn.write(password + "\n")
-            pat_id,m,buffer = self.tn.expect(prompt_pattern_list)
-            if pat_id < 0:
-                raise NotConnected('No regular prompt found.')
+        self.prompt = prompt
+        self.logout_steps = logout_steps
+        self.context
+        self.timeout = timeout
+        with Timeout(seconds = timeout, error_message='Timeout (%ss) for pexpect to %s' % (timeout,host)):
+            self.child = pexpect.spawn(spawn)
+            self._expect_steps(login_steps)
             self.is_connected = True
+
+    def _expect_steps(self,steps):
+        step = 0
+        nb_steps = len(steps)
+        while step < nb_steps:
+            expects = steps[step]
+            nb_base_expects = len(expects)
+            if step+1 < nb_steps:
+                expects += steps[step+1]
+            print 'self.child.expect(',[ e[0] for e in expects ],')'
+            found = self.child.expect([ e[0] for e in expects ])
+            print '  --> found =',found
+            to_send = expects[found][1]
+            if to_send is not None:
+                if isinstance(to_send,basestring):
+                    to_send = to_send.format(**self.context)
+                    if to_send and to_send[-1] == '\n':
+                        self.child.sendline(to_send[:-1])
+                    else:
+                        self.child.send(to_send)
+                elif to_send == Expect.KILL:
+                    self.child.kill(0)
+            if found >= nb_base_expects or step+1 == nb_steps:
+                step += 1
 
     def __enter__(self):
         self.in_with = True
@@ -98,18 +113,18 @@ class Expect(object):
 
     def close(self):
         if not self.in_with:
-            self.tn.close()
             self.is_connected = False
+            self._expect_steps(self.logout_steps)
 
     def _run_cmd(self,cmd):
-        self.tn.write('%s\n' % cmd)
-        pat_id,m,buffer = self.tn.expect(self.prompt_pattern_list)
-        out = buffer.splitlines()[1:-1]
-        return '\n'.join(out)
+        self.child.sendline('%s\n' % cmd)
+        self.child.expect(self.prompt)
+        out = self.child.before
+        return out
 
     def run(self, cmd, timeout=30, **kwargs):
         if not self.is_connected:
-            raise NotConnected('No telnet connection to run your command.')
+            raise NotConnected('No expect connection to run your command.')
         out = None
         try:
             with Timeout(seconds = timeout):
@@ -121,7 +136,7 @@ class Expect(object):
 
     def mrun(self, cmds, timeout=30, **kwargs):
         if not self.is_connected:
-            raise NotConnected('No telnet connection to run your command.')
+            raise NotConnected('No expect connection to run your command.')
         dct = textops.DictExt()
         if isinstance(cmds,dict):
             cmds = cmds.items()
@@ -135,7 +150,7 @@ class Expect(object):
         return dct
 
 class Telnet(object):
-    def __init__(self,host, user, password=None, timeout=10, port=0, login_pattern_list=None, passwd_pattern_list=None, prompt_pattern_list=None,*args,**kwargs):
+    def __init__(self,host, user, password=None, timeout=30, port=0, login_pattern_list=None, passwd_pattern_list=None, prompt_pattern_list=None,*args,**kwargs):
         #import is done only on demand, because it takes some little time
         import telnetlib
         self.in_with = False
@@ -206,7 +221,7 @@ class Telnet(object):
         return dct
 
 class Ssh(object):
-    def __init__(self,host, user, password=None, timeout=10, *args,**kwargs):
+    def __init__(self,host, user, password=None, timeout=30, *args,**kwargs):
         #import is done only on demand, because it takes some little time
         import paramiko
         self.in_with = False
