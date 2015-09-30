e44c619ea97c35956281fef471b4f880759ee9e2
elapouya <elapouya@gmail.com>
2015-09-30 13:46:40 +0200

    new __version__ management

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
 import os
+import re
 
 def read(*names):
     values = dict()
@@ -23,8 +24,16 @@ News
 %(CHANGES)s
 """ % read('README', 'CHANGES')
 
+def get_version(pkg):
+    path = os.path.join(os.path.dirname(__file__),pkg,'__init__.py')
+    with open(path) as fh:
+        m = re.search(r'^__version__\s*=\s*[\'"]([^\'"]+)[\'"]',fh.read(),re.M)
+    if m:
+        return m.group(1)
+    raise RuntimeError("Unable to find __version__ string in %s." % path)
+
 setup(name='python-nagios-helpers',
-      version='0.0.5',
+      version=get_version('naghelp'),
       description='Helper Classes for plugin development and nagios management.',
       long_description=long_description,
       classifiers=[
@@ -42,8 +51,8 @@ setup(name='python-nagios-helpers',
       install_requires = ['Sphinx<1.3b',
                           'sphinxcontrib-napoleon',
                           'paramiko',
+                          'pexpect',
                           'python-textops',
-                          'addicted',
                           ],
       eager_resources = ['docs'],
       zip_safe=False)
