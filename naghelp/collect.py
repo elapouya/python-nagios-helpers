9ce6ed00e905357c75eace283df89383e4666c86
Eric Lapouyade <elapouya@gmail.com>
2015-12-01 17:12:38 +0100

    Add docs

--- naghelp/collect.py
+++ naghelp/collect.py
@@ -14,7 +14,7 @@ import textops
 import naghelp
 import time
 
-__all__ = ['search_invalid_port', 'runsh', 'mrunsh', 'Expect', 'Telnet', 'Ssh', 'Snmp', 'SnmpError', 
+__all__ = ['search_invalid_port', 'runsh', 'mrunsh', 'Expect', 'Telnet', 'Ssh', 'Snmp', 'SnmpError',
            'Timeout', 'TimeoutError', 'CollectError', 'ConnectionError', 'NotConnected']
 
 class NotConnected(Exception):
@@ -77,49 +77,137 @@ def debug_pattern_list(pat_list):
     return [ (pat if isinstance(pat,basestring) else pat.pattern) for pat in pat_list ]
 
 class Expect(object):
-    """ Simulate human action on console
-    
-    :class:`Expect` is a class that "talks" to other interactive programs. 
+    r""" Interact with a spawn command
+
+    :class:`Expect` is a class that "talks" to other interactive programs.
     It is based on `pexpect <https://pexpect.readthedocs.org>`_ and is
-    focused on running one or many commands. :class:`Expect` is to be used when :class:`Telnet` and 
+    focused on running one or many commands. :class:`Expect` is to be used when :class:`Telnet` and
     :class:`Ssh` are not applicable.
-    
+
     Args:
-    
+
         spawn (str): The command to start and to communicate with
         login_steps(list or tuple): steps to execute to reach a prompt
         prompt (str): A pattern that matches the prompt
-        logout_steps(list or tuple): steps to execute before closing communication
-        context (dict): Dictionary that will be used in steps to .format() strings
+        logout_cmd(str): command to execute before closing communication (Default : None)
+        logout_steps(list or tuple): steps to execute before closing communication (Default : None)
+        context (dict): Dictionary that will be used in steps to .format() strings (Default : None)
         timeout (int): Maximum execution time (Default : 30)
-        
-    """    
+
+    On object creation, :class:`Expect` will :
+
+        * spawn the specified command (``spawn``)
+        * follow ``login_steps``
+        * and wait for the specified ``prompt``.
+
+    on object :meth:`run` or :meth:`mrun`, it will :
+
+        * execute the specified command(s)
+        * wait the specified prompt between each command
+        * return command(s) output wihout prompt string
+        * then close the interaction (see just below)
+
+    on close, :class:`Expect` will :
+
+        * execute the ``logout_cmd``
+        * follow ``logout_steps``
+        * finally terminate the spawned command
+
+    **What are Steps ?**
+
+        During login and logout, one can specify steps. A step is one or more pattern/answer tuples.
+        The main tuple syntax is::
+
+            (
+                (
+                    ( step1_pattern1, step1_answer1),
+                    ( step1_pattern2, step1_answer2),
+                    ...
+                ),
+                (
+                    ( step2_pattern1, step2_answer1),
+                    ( step2_pattern2, step2_answer2),
+                    ...
+                ),
+                ...
+            )
+
+        For a same step, :class:`Expect` will search for any of the specified patterns, then will respond
+        the corresponding answer. It will go to the next step only when one of the next step's patterns
+        is found. If not, will stay at the same step looking again for any of the patterns.
+        In order to simplify tuple expression, if there is only one tuple in a level,
+        the parenthesis can be removed. For answer, you have to specify a string : do not forget
+        the newline otherwise you will get stuck.
+        One can also use ``Expect.BREAK`` to stop following the steps, ``Expect.RESPONSE``
+        to raise an error with the found pattern as message. ``Expect.KILL`` does the same but also
+        kills the spawned command.
+
+        Here are some ``login_steps`` examples :
+
+        The spawned command is just waiting a password::
+
+            (r'(?i)Password[^:]*: ','wwwpw\n')
+
+        The spawned command is waiting a login then a password::
+
+            (
+                (r'(?i)Login[^:]*: ','www\n'),
+                (r'(?i)Password[^:]*: ','wwwpassword\n')
+            )
+
+        The spawned command is waiting a login then a password, but may ask a question at login prompt::
+
+            (
+                (
+                    (r'(?i)Are you sure to connect \?','yes'),
+                    (r'(?i)Login[^:]*: ','www\n')
+                ),
+                ('(?i)Password[^:]*: ','wwwpassword\n')
+            )
+
+        You can specify a context dictionary to :class:`Expect` to format answers strings.
+        With ``context = { 'user':'www','passwd':'wwwpassword' }`` login_steps becomes::
+
+            (
+                (
+                    (r'(?i)Are you sure to connect \?','yes'),
+                    (r'(?i)Login[^:]*: ','{user}\n')
+                ),
+                ('(?i)Password[^:]*: ','{passwd}\n')
+            )
+    """
     KILL = 1
     BREAK = 2
     RESPONSE = 3
 
-    def __init__(self,spawn,login_steps,prompt,logout_steps=None,context={},timeout = 30,*args,**kwargs):
-        global pexpect
+    def __init__(self,spawn,login_steps=None,prompt=None,logout_cmd=None,logout_steps=None,context={},timeout = 30,*args,**kwargs):
+        # normalizing : transform tuple of string into tuple of tuples of string
+        if login_steps and isinstance(login_steps[0],basestring):
+            login_steps = (login_steps,)
+        if logout_steps and isinstance(logout_steps[0],basestring):
+            logout_steps = (logout_steps,)
+
+        # Copy all params as object attributes
+        self.__dict__.update(locals())
 
         #import is done only on demand, because it takes some little time
+        global pexpect
         import pexpect
         self.in_with = False
         self.is_connected = False
-        self.spawn = spawn
-        self.prompt = prompt
-        self.logout_steps = logout_steps
-        self.context = context
-        self.timeout = timeout
         naghelp.logger.debug('#### Expect( %s ) ###############',spawn)
         with Timeout(seconds = timeout, error_message='Timeout (%ss) for pexpect : %s' % (timeout,spawn)):
             self.child = pexpect.spawn(spawn)
-            naghelp.logger.debug('==== Login steps up to the prompt =====')
-            error_msg = self._expect_steps( login_steps + ((prompt,None),) )
-            if error_msg:
-                raise ConnectionError(error_msg)
+            if login_steps or prompt:
+                naghelp.logger.debug('==== Login steps up to the prompt =====')
+                error_msg = self._expect_steps( (login_steps or ()) + ( ((prompt,None),) if prompt else () ) )
+                if error_msg:
+                    raise ConnectionError(error_msg)
             self.is_connected = True
 
     def _expect_pattern_rewrite(self,pat):
+        if pat is None:
+            return pexpect.EOF
         pat = re.sub(r'^\^',r'[\r\n]',pat)
         return pat
 
@@ -194,17 +282,25 @@ class Expect(object):
     def close(self):
         if not self.in_with:
             self.is_connected = False
+            if self.logout_cmd:
+                self.child.sendline(self.logout_cmd)
             if self.logout_steps:
                 self._expect_steps(self.logout_steps)
-            self.child.kill(0)
+            try:
+                self.child.kill(0)
+            except OSError:
+                pass
             naghelp.logger.debug('#### Expect : Connection closed ###############')
 
     def _run_cmd(self,cmd):
-        naghelp.logger.debug('  ==> sendline : %s',cmd)
-        self.child.sendline('%s' % cmd)
-        naghelp.logger.debug('<-- expect prompt ...')
+        if cmd:
+            naghelp.logger.debug('  ==> sendline : %s',cmd)
+            self.child.sendline('%s' % cmd)
+
+        prompt = self._expect_pattern_rewrite(self.prompt)
+        naghelp.logger.debug('<-- expect prompt : %s',prompt)
         try:
-            self.child.expect(self._expect_pattern_rewrite(self.prompt))
+            self.child.expect(prompt)
         except pexpect.EOF:
             naghelp.logger.debug('CollectError : No more data (EOF) from %s' % self.spawn)
             raise CollectError('No more data (EOF) from %s' % self.spawn)
@@ -216,8 +312,45 @@ class Expect(object):
         out = out.replace('\r','')
         return out
 
-    def run(self, cmd, timeout=30, auto_close=True, **kwargs):
-        """ Execute one command """
+    def run(self, cmd=None, timeout=30, auto_close=True, **kwargs):
+        r""" Execute one command
+
+        Runs a single command at the specified prompt and then close the interaction. Timeout
+        will not raise any error but will return None.
+        If you want to execute many commands without closing the interation, use ``with`` syntax.
+
+        Args:
+
+            cmd (str): The command to be executed by the spawned command
+            timeout (int): A timeout after which the result will be None
+            auto_close (bool): Automatically close the interaction.
+
+        Return:
+
+            str : The command output or None on timeout
+
+        Examples:
+
+            SSH::
+
+                e = Expect('ssh www@localhost',
+                            login_steps=('(?i)Password[^:]*: ','wwwpassword\n'),
+                            prompt=r'www@[^\$]*\$ ',
+                            logout_cmd='exit')
+                print e.run('ls -la')
+
+            SSH with multiple commands::
+
+                with Expect('ssh www@localhost',
+                            login_steps=('(?i)Password[^:]*: ','wwwpassword\n'),
+                            prompt=r'www@[^\$]*\$ ',
+                            logout_cmd='exit') as e:
+                    cur_dir = e.run('pwd').strip()
+                    big_files_full_path = e.run('find %s -type f -size +10000' % cur_dir)
+                print big_files_full_path
+
+
+        """
         if not self.is_connected:
             raise NotConnected('No expect connection to run your command.')
         out = None
@@ -231,7 +364,40 @@ class Expect(object):
         return out
 
     def mrun(self, cmds, timeout=30, auto_close=True, **kwargs):
-        """ Execute many commands at the same time """
+        r""" Execute many commands at the same time
+
+        Runs a dictionary of commands at the specified prompt and then close the interaction.
+        Timeout will not raise any error but will return None for the running command.
+        It returns a dictionary where keys are the same as the ``cmds`` dict and the values are
+        the commmands output.
+
+        Args:
+
+            cmds (dict or list of items): The commands to be executed by the spawned command
+            timeout (int): A timeout after which the result will be None
+            auto_close (bool): Automatically close the interaction.
+
+        Return:
+
+            dict : The commands output
+
+        Example:
+
+            SSH with multiple commands::
+
+                e = Expect('ssh www@localhost',
+                            login_steps=('(?i)Password[^:]*: ','wwwpassword\n'),
+                            prompt=r'www@[^\$]*\$ ',
+                            logout_cmd='exit')
+                print e.mrun({'cur_dir':'pwd','big_files':'find . -type f -size +10000'})
+
+            Will return something like::
+
+                {
+                    'cur_dir' : '/home/www',
+                    'big_files' : 'bigfile1\nbigfile2\nbigfile3\n...'
+                }
+        """
         if not self.is_connected:
             raise NotConnected('No expect connection to run your command.')
         dct = textops.DictExt()
@@ -250,39 +416,63 @@ class Expect(object):
         return dct
 
 class Telnet(object):
-    """ Telnet class helper """
-    def __init__(self,host, user, password=None, timeout=30, port=0, login_pattern_list=None, passwd_pattern_list=None, prompt_pattern=None,*args,**kwargs):
+    r""" Telnet class helper
+
+    This class create a telnet connection in order to run one or many commands.
+
+    Args:
+
+        host (str): IP address or hostname to connect to
+        user (str): The username to use for login
+        password (str): The password
+        timeout (int): Time in seconds before raising an error or a None value
+        port (int): port number to use (Default : 0 = 23)
+        login_pattern (str or list): The pattern to recognize the login prompt
+            (Default : ``login\s*:``). One can specify a string, a re.RegexObject,
+            a list of string or a list of re.RegexObject
+        passwd_pattern (str or list): The pattern to recognize the password prompt
+            (Default : ``Password\s*:``). One can specify a string, a re.RegexObject,
+            a list of string or a list of re.RegexObject
+        prompt_pattern (str): The pattern to recognize the usual prompt
+            (Default : ``[\r\n][^\s\$#<>:]*\s?[\$#>:]+\s``). One can specify a string or a re.RegexObject.
+    """
+    def __init__(self,host, user, password=None, timeout=30, port=0, login_pattern=None, passwd_pattern=None, prompt_pattern=None,*args,**kwargs):
         #import is done only on demand, because it takes some little time
         import telnetlib
         self.in_with = False
         self.is_connected = False
         self.prompt = None
-        if login_pattern_list is None:
-            login_pattern_list = [re.compile(r'login\s*:',re.I),]
-        if passwd_pattern_list is None:
-            passwd_pattern_list = [re.compile(r'Password\s*:',re.I),]
-        if prompt_pattern is None:
-            prompt_pattern_list = [re.compile(r'[\r\n][^\s\$#<>:]*\s?[\$#>:]+\s'),]
-        else:
-            prompt_pattern_list = [re.compile(re.sub(r'^\^',r'[\r\n]',prompt_pattern))]
-        self.prompt_pattern_list = prompt_pattern_list
+        login_pattern = Telnet._normalize_pattern(login_pattern, r'login\s*:')
+        passwd_pattern = Telnet._normalize_pattern(passwd_pattern, r'Password\s*:')
+        prompt_pattern = Telnet._normalize_pattern(prompt_pattern, r'[\r\n][^\s\$#<>:]*\s?[\$#>:]+\s')
+        self.prompt_pattern = prompt_pattern
         naghelp.logger.debug('#### Telnet( %s@%s ) ###############',user, host)
         with Timeout(seconds = timeout, error_message='Timeout (%ss) for telnet to %s' % (timeout,host)):
             self.tn = telnetlib.Telnet(host,port,timeout,**kwargs)
-            naghelp.logger.debug('<-- expect(%s) ...',debug_pattern_list(login_pattern_list))
-            self.tn.expect(login_pattern_list)
+            naghelp.logger.debug('<-- expect(%s) ...',debug_pattern_list(login_pattern))
+            self.tn.expect(login_pattern)
             naghelp.logger.debug('  ==> %s',user)
             self.tn.write(user + "\n")
-            naghelp.logger.debug('<-- expect(%s) ...',debug_pattern_list(passwd_pattern_list))
-            self.tn.expect(passwd_pattern_list)
+            naghelp.logger.debug('<-- expect(%s) ...',debug_pattern_list(passwd_pattern))
+            self.tn.expect(passwd_pattern)
             naghelp.logger.debug('  ==> (hidden password)')
             self.tn.write(password + "\n")
-            naghelp.logger.debug('<-- expect(%s) ...',debug_pattern_list(prompt_pattern_list))
-            pat_id,m,buffer = self.tn.expect(prompt_pattern_list)
+            naghelp.logger.debug('<-- expect(%s) ...',debug_pattern_list(prompt_pattern))
+            pat_id,m,buffer = self.tn.expect(prompt_pattern)
             if pat_id < 0:
                 raise ConnectionError('No regular prompt found.')
             naghelp.logger.debug('Prompt found : is_connected = True')
             self.is_connected = True
+
+    @staticmethod
+    def _normalize_pattern(pattern,default):
+        if pattern is None:
+            pattern = [re.compile(default,re.I)]
+        elif isinstance(pattern,basestring):
+            pattern = [re.sub(r'^\^',r'[\r\n]',pattern)]
+        elif not isinstance(pattern,list):
+            pattern = [pattern]
+        return pattern
 
     def __enter__(self):
         self.in_with = True
@@ -301,8 +491,8 @@ class Telnet(object):
     def _run_cmd(self,cmd):
         naghelp.logger.debug('  ==> %s',cmd)
         self.tn.write('%s\n' % cmd)
-        naghelp.logger.debug('<-- expect(%s) ...',debug_pattern_list(self.prompt_pattern_list))
-        pat_id,m,buffer = self.tn.expect(self.prompt_pattern_list)
+        naghelp.logger.debug('<-- expect(%s) ...',debug_pattern_list(self.prompt_pattern))
+        pat_id,m,buffer = self.tn.expect(self.prompt_pattern)
         out = buffer.replace('\r','')
         # use re.compile to be compatible with python 2.6 (flags in re.sub only for python 2.7+)
         rmcmd = re.compile(r'^.*?%s\n*' % cmd, re.DOTALL)
@@ -312,6 +502,46 @@ class Telnet(object):
         return '\n'.join(out)
 
     def run(self, cmd, timeout=30, auto_close=True, **kwargs):
+        r""" Execute one command
+
+        Runs a single command at the usual prompt and then close the connection. Timeout
+        will not raise any error but will return None.
+        If you want to execute many commands without closing the interation, use ``with`` syntax.
+
+        Args:
+
+            cmd (str): The command to be executed
+            timeout (int): A timeout after which the result will be None
+            auto_close (bool): Automatically close the connection.
+
+        Return:
+
+            str : The command output or None on timeout
+
+        Examples:
+
+            Telnet with default login/password/prompt::
+
+                tn = Telnet('localhost','www','wwwpassword')
+                print tn.run('ls -la')
+
+            Telnet with custom password prompt (password in french), note the ``(?i)`` for the case insensitive::
+
+                tn = Telnet('localhost','www','wwwpassword',password_pattern=r'(?i)Mot de passe\s*:')
+                print tn.run('ls -la')
+
+            Telnet with multiple commands::
+
+                with Expect('ssh www@localhost',
+                            login_steps=('(?i)Password[^:]*: ','wwwpw\n'),
+                            prompt=r'www@[^\$]*\$ ',
+                            logout_cmd='exit') as e:
+                    cur_dir = e.run('pwd').strip()
+                    big_files_full_path = e.run('find %s -type f -size +10000' % cur_dir)
+                print big_files_full_path
+
+
+        """
         if not self.is_connected:
             raise NotConnected('No telnet connection to run your command.')
         out = None
