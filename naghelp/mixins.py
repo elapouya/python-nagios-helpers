c0fd6677bf4b107db732545c90b533507955a5f4
Eric Lapouyade <elapouya@gmail.com>
2016-03-18 15:47:59 +0100

    update Gauge mixin

--- naghelp/mixins.py
+++ naghelp/mixins.py
@@ -13,22 +13,47 @@ class GaugeException(Exception):
     pass
 
 class GaugeMixin(object):
+    def gauge_response_threshold_list(self,id,label_values,warn_min=None,crit_min=None,warn_max=None,crit_max=None):
+        for i,(label,value) in enumerate(label_values):
+            self.gauge_response_threshold('%s%s' % (id,i),label,value,warn_min=warn_min,crit_min=crit_min,warn_max=warn_max,crit_max=crit_max)
+
+    def gauge_response_factor_list(self,id,label_values,warn_min=None,crit_min=None,warn_max=None,crit_max=None):
+        for i,(label,value) in enumerate(label_values):
+            self.gauge_response_factor('%s%s' % (id,i),label,value,warn_min=warn_min,crit_min=crit_min,warn_max=warn_max,crit_max=crit_max)
+
     def gauge_response_threshold(self,id,label,value,warn_min=None,crit_min=None,warn_max=None,crit_max=None):
         self.response.add_more('%s : %s',label,value)
         self.debug('Gauge id=%s, value=%s (warn_min=%s,crit_min=%s,warn_max=%s,crit_max=%s)',id,value,warn_min,crit_min,warn_max,crit_max)        
         if isinstance(value,basestring):
-            value = (value|find_pattern(r'([\d,\.]+)')).replace(',','.')
+            value = find_pattern.op(value,r'([\d,\.]+)').replace(',','.')
+            if value:
+                value=float(value)
+        if isinstance(value,(int,float)): 
+            if isinstance(crit_min,(int,float)) and value <= crit_min:
+                self.response.add(CRITICAL,'%s : %s <= MIN CRITICAL (%s)' % (label, value, crit_min))
+            elif isinstance(warn_min,(int,float)) and value <= warn_min:
+                self.response.add(WARNING,'%s : %s <= MIN WARNING (%s)' % (label, value, warn_min))
+            elif isinstance(crit_max,(int,float)) and value >= crit_max:
+                self.response.add(CRITICAL,'%s : %s >= MAX CRITICAL (%s)' % (label, value, crit_max))
+            elif isinstance(warn_max,(int,float)) and value >= warn_max:
+                self.response.add(WARNING,'%s : %s >= MAX WARNING (%s)' % (label, value, warn_max))
+            
+    def gauge_response_factor(self,id,label,value,warn_min=None,crit_min=None,warn_max=None,crit_max=None):
+        self.response.add_more('%s : %s',label,value)
+        self.debug('Gauge id=%s, value=%s (warn_min=%s,crit_min=%s,warn_max=%s,crit_max=%s)',id,value,warn_min,crit_min,warn_max,crit_max)        
+        if isinstance(value,basestring):
+            value = find_pattern.op(value,r'([\d,\.]+)').replace(',','.')
             if value:
                 value=float(value)
         if isinstance(value,(int,float)):            
-            if isinstance(crit_min,(int,float)) and value <= crit_min:
-                self.response.add(CRITICAL,'%s : actual value (%s) is less than or equal to the critical value (%s)' % (label, value, crit_min))
-            elif isinstance(warn_min,(int,float)) and value <= warn_min:
-                self.response.add(WARNING,'%s : actual value (%s) is less than or equal to the warning value (%s)' % (label, value, warn_min))
-            elif isinstance(crit_max,(int,float)) and value >= crit_max:
-                self.response.add(CRITICAL,'%s : actual value (%s) is more than or equal to the critical value (%s)' % (label, value, crit_max))
-            elif isinstance(warn_max,(int,float)) and value >= warn_max:
-                self.response.add(WARNING,'%s : actual value (%s) is more than or equal to the warning value (%s)' % (label, value, warn_max))
+            if isinstance(crit_min,(int,float)) and value <= value*crit_min:
+                self.response.add(CRITICAL,'%s : %s <= MIN CRITICAL (%s)' % (label, value, value*crit_min))
+            elif isinstance(warn_min,(int,float)) and value <= value*warn_min:
+                self.response.add(WARNING,'%s : %s <= MIN WARNING (%s)' % (label, value, value*warn_min))
+            elif isinstance(crit_max,(int,float)) and value >= value*crit_max:
+                self.response.add(CRITICAL,'%s : %s >= MAX CRITICAL (%s)' % (label, value, value*crit_max))
+            elif isinstance(warn_max,(int,float)) and value >= value*warn_max:
+                self.response.add(WARNING,'%s : %s >= MAX WARNING (%s)' % (label, value, value*warn_max))
             
     def gauge_response_etalon_change(self,id,label,value,level):
         self.response.add_more('%s : %s',label,value)
